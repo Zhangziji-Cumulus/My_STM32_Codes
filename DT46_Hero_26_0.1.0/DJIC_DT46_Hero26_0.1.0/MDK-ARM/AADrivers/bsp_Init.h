d270dbd0e7000b0fd9a0edef5a1483d6ea59a29b
Zhangziji-Cumulus <3363997071@qq,com>
2026-05-15 20:02:54 +0800

    写了can和sbus驱动层

--- DT46_Hero_26_0.1.0/DJIC_DT46_Hero26_0.1.0/MDK-ARM/AADrivers/bsp_Init.h
+++ DT46_Hero_26_0.1.0/DJIC_DT46_Hero26_0.1.0/MDK-ARM/AADrivers/bsp_Init.h
@@ -1,19 +1,16 @@
 #ifndef __BSP_INIT_H__
 #define __BSP_INIT_H__
 
-/* system includes */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usart.h"
-
-/* user includes */
 #include "bsp_CAN.h"
 #include "bsp_SBUS.h"
 
 /* Exported functions --------------------------------------------------------*/
 
 /**
- * @brief 初始化驱动层函数
- * 
+ * @brief 初始化底层驱动模块
  */
 void BSP_Init(void);
 
