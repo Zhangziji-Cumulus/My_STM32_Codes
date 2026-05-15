56ca9961c08a091c1664f559758ec0f3375a47a5
Zhangziji-Cumulus <3363997071@qq,com>
2026-05-15 17:11:59 +0800

    优化代码0515

---
+++ DT46_Hero_26_0.1.0/DJIC_DT46_Hero26_0.1.0/MDK-ARM/AADrivers/bsp_Init.h
@@ -0,0 +1,6 @@
+#ifndef __BSP_INIT_H__
+#define __BSP_INIT_H__
+
+
+
+#endif /* __BSP_INIT_H__ */
--- DT46_Hero_26_0.1.0/DJIC_DT46_Hero26_0.1.0/MDK-ARM/BaseMood/ist8310driver.d
+++
@@ -1,4 +0,0 @@
-basemood\ist8310driver.o: UAP_IMU\ist8310driver.c
-basemood\ist8310driver.o: UAP_IMU\ist8310driver.h
-basemood\ist8310driver.o: UAP_IMU\struct_typedef.h
-basemood\ist8310driver.o: UAP_IMU\ist8310driver_middleware.h
